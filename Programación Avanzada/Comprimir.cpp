81c920dcb7c7f2249e190d7deb3dc25b1d624659
Lalito Robles <laloro08@hotmail.com>
2020-08-27 10:59:32 -0500

    27 de agosto

--- Programación Avanzada/Comprimir.cpp
+++ Programación Avanzada/Comprimir.cpp
@@ -5,19 +5,30 @@ int main(){
   cout << "Ingrese una cadena de texto: ";
   char cad[100];
   cin.getline(cad, 100, '\n');
-  int n = strlen(cad), c = 1;
+  int n = strlen(cad), c = 1, p = 1;
   for( int i = 1; i < n; i++ ){
-    if( cad[i] == cad[i-1] ){
+    if( cad[i] == cad[i-1] && i < n-1 ){
       c++;
     }
-    if( c > 1 && (cad[i] != cad[i-1] || i+1 == n) ){
-      if( c > 9 ){
-	cad[i+1-c] = c / 10 + 48;
-	cad[i+2-c] = c % 10 + 48;
+    else{
+      cad[p++] = cad[i];
+      if( c > 1 ){
+	cad[p] = c;
+	if( c > 9 ) p++;
+	p++;
+	c = 1;
       }
-      else cad[i+1-c] = c + 48;
-      c = 1;
     }
+//    if( c > 1 && (cad[i] != cad[i-1] || i+1 == n) ){
+//      if( c > 9 ){
+//      cad[i+1-c] = c / 10 + 48;
+//      cad[i+2-c] = c % 10 + 48;
+//  }
+//  else cad[i+1-c] = c + 48;
+//  	c = 1;
+//      cad[p++] = c;
+//      c = 1;
+//    }
   }
   cout << cad;
   return 0;
