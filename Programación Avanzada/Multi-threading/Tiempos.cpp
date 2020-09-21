9d0efbb7123266e3fe54ff71746b9dfb1bef48ec
Lalito Robles <laloro08@hotmail.com>
2020-09-21 12:15:22 -0500

    21 de Septiembre

---
+++ Programación Avanzada/Multi-threading/Tiempos.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <chrono>
+using namespace std;
+#define N_THREADS 5
+void print(int n, const string &str){
+  string msg = to_string(n) + " " + str;
+  cout << msg << endl;
+  this_thread::sleep_for(chrono::milliseconds(10000));
+}
+int main(){
+  string s[] = {"IIA", "IB", "IM", "II", "ITE"};
+  thread threads[N_THREADS];
+  for( int i = 0; i < N_THREADS; i++ ){
+    threads[i] = thread(print, i, s[i]);
+    threads[i].join();
+  }
+  return 0;
+}
