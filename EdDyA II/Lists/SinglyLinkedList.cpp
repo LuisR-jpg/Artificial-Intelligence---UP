48b3343c09aa51d074f7cf1b2f0bc6be6827d132
Lalito Robles <laloro08@hotmail.com>
2020-09-24 12:24:52 -0500

    24 de Septiembre

--- EdDyA II/Lists/SinglyLinkedList.cpp
+++ EdDyA II/Lists/SinglyLinkedList.cpp
@@ -1,24 +1,25 @@
 //Enteros
 #include <iostream>
 using namespace std;
-class Node{
-  public:
-    int data;
-    Node *next;
-    Node(int data){
-      cout << "\tNode(" << data << ")" << endl;
-      this -> data = data;
-      this -> next = NULL; //nullptr
-    }
-    ~Node(){
-      cout << "\t~Node(" << data << ")" << endl;
-    }
-    friend ostream& operator << (ostream& output, const Node &n){
-      output << "Data: " << n.data << "\tNext: " << n.next << endl;
-    }
-};
 class SinglyLinkedList{
   public:
+    class Node{
+      public:
+        int data;
+        Node *next;
+        Node(int data){
+          cout << "\tNode(" << data << ")" << endl;
+          this -> data = data;
+          this -> next = NULL; //nullptr
+        }
+        ~Node(){
+          cout << "\t~Node(" << data << ")" << endl;
+        }
+        friend ostream& operator << (ostream& output, const Node &n){
+          output << "Data: " << n.data << "\tNext: " << n.next << endl;
+          return output;
+        }
+    };
     Node* head;
     SinglyLinkedList(){
       head = NULL;
@@ -27,35 +28,65 @@ class SinglyLinkedList{
     ~SinglyLinkedList(){
       cout << "\t~SinglyLinkedList()" << endl;
       Node *a = head;
-      while( a != NULL ){
+      while( a ){
 	Node *b = a -> next;
 	delete a;
 	a = b;
       }
     }
+    void insert_head(int data){
+      Node *n = new Node(data);
+      if( head ) n -> next = head;
+      head = n;
+    }
     void insert_tail(int data){
       Node *n = new Node(data);
-      if( head == NULL ) head = n;
+      if( !head ) head = n;
       else{
         Node *a = head;
         while( a -> next != NULL ) a = a -> next;
         a -> next = n;
       }
     }
+    void remove(int data){
+      if( !head ) return;
+      if( data == head -> data ){
+	Node *a = head;
+	head = head -> next;
+	delete a;
+      }
+      else{
+	Node *a = head;
+	while( a -> next && a -> next -> data != data )
+	  a = a -> next;
+	if( a -> next ){
+	  Node *b = a -> next;
+	  a -> next = a -> next -> next;
+	  delete b;
+	}
+      }
+    }
     friend ostream &operator << (ostream &output, const SinglyLinkedList &l){
       Node *n = l.head;
-      while( n != NULL ){
+      while( n ){
 	output << *n;
 	n = n -> next;
       }
-//      output << endl;
+      return output;
     }
 };
 int main(){
   SinglyLinkedList lista;
-  lista.insert_tail(1);
-  lista.insert_tail(2);
-  lista.insert_tail(3);
+  lista.insert_head(1);
+  lista.insert_head(2);
+  lista.insert_head(3);
+//  cout << lista;
+//  lista.remove(2);
+//  lista.remove(5);
+//  lista.remove(3);
+//  lista.remove(1);
+//  lista.remove(5);
   cout << lista;
   return 0;
 }
+
