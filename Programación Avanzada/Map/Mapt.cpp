518b060d26479b00ac00cf035e4270f9a6ed1bbf
Lalito Robles <laloro08@hotmail.com>
2020-10-14 17:23:19 -0500

    14 de Octubre

--- Programación Avanzada/Map/Mapt.cpp
+++ Programación Avanzada/Map/Mapt.cpp
@@ -10,26 +10,28 @@ class Person{
   public:
     Person():name(""), age(0){}
     Person(string name, int age): name(name), age(age){}
-    void print(){
-      cout << name << ": " << age << endl;
+    void print() const{
+      cout << name << ": " << age;
     }
-    bool operator < (const Persona &other) const{
+    bool operator < (const Person &other) const{
       if(name == other.name) return age < other.age;
       else return name < other.name;
     }
 };
 int main(){
   map<Person, int> people;
+  /*
   string au = "Hola"; 
   string ad = "HoaZ";
   bool a = au < ad; //Son comparables
   bool r = Personau < Personad; //No implementado: Sobrecarga de operadores is needed
+  */
   people[Person("Cristi", 18)] = 40;
   people[Person("Fabrizio", 19)] = 400;
   people[Person("Sara", 1800)] = 4;
   for(map<Person, int>::iterator it = people.begin(); it != people.end(); it++){
     it -> first.print();
-    cout << it -> second << endl;
+    cout << ": " << it -> second << endl;
   }
   return 0;
 }
