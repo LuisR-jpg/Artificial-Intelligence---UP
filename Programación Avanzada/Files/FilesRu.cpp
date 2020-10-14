69738283c2c59432aca22040b15dff0e6086c693
Lalito Robles <laloro08@hotmail.com>
2020-10-13 22:31:12 -0500

    13 de Octubre

--- Programación Avanzada/Files/FilesRu.cpp
+++ Programación Avanzada/Files/FilesRu.cpp
@@ -1,42 +1,59 @@
 #include <iostream>
-#include <string.h>
+#include <string>
 #include <fstream>
+#include <vector>
 using namespace std;
-int arr[100000] = {0};
+int arr[1000000] = {0};
+vector<int> va, vb;
 void llenar(){
   ifstream jaja;
   for(int i = 0; i < 2; i++ ){
     if( !i ) jaja.open("f1.txt", ios::in);
-    else jaja.open("f2.txt", ios::in);
+    else{
+      jaja.close();
+      jaja.open("f2.txt", ios::in);
+    }
     string a;
     while( !jaja.eof() ){
       getline(jaja, a);
-      int b = 0;
-      cout << "\t\t" << sizeof(a)/sizeof(a[0]) << endl;
-      for( int i = 0; i < sizeof(a)/sizeof(a[0]); i++ ){
-	if( a[i] == ':' ){
-	  int c = 0;
-	  b /= 10;
-	  while( b ){
-	    c += b % 10;
-	    c *= 10, b /= 10;
-	  }
-	  c /= 10;
-	  cout << "\t" << c << endl;
-//	  arr[c]++, b = 0;
-	}
-	else{
-	  b += a[i] - '0'; 
-	  b *= 10;
+      int l = 0;
+      for(int j = 0; j < a.size(); j++){
+	if(a[j] == ':' || j == a.size() - 1){
+	  long b = stol(a.substr(l, j - l + (j == a.size() - 1? 1: 0)));
+	  l = j + 1;
+	  arr[b]++;
+	  if( !i ) va.push_back(b);
+	  else vb.push_back(b);
 	}
       }
+      if( !i ) va.push_back(-1);
+      else vb.push_back(-1);
     }
   }
 }
 int main(){
   llenar();
-  for( int i = 0; i < 1000000; i++ )
-    if( arr[i] ) cout << arr[i] << " ";
+//  for(int i = 0; i < va.size() - 1; i++){
+//    cout << va[i] << " ";
+//  }
+//  cout << endl;
+//  for(int i = 0; i < vb.size() - 1; i++){
+//    cout << vb[i] << " ";
+//  }
+//  cout << endl;
+  
+  ofstream jaja;
+  jaja.open("f3.txt", ios::out);
+  if( jaja.fail() ){
+    cout << "No se abrió el archivo." << endl;
+    return 0;
+  }
+  int a = 0, b = 0;
+  while(a < va.size() || b < vb.size()){
+    while(va[a++] != -1 && arr[va[a-1]] == 1 ) jaja << va[a-1] << ":";
+    while(vb[b++] != -1 && arr[va[b-1]] == 1 ) jaja << vb[b-1] << ":";
+    jaja << endl;
+  }
   return 0;
 }
   
