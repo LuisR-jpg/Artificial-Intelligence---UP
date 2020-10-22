985e4f77cdb30cc1e5dbc5f56650979a1fe99c3e
LuisR-jpg <laloro08@hotmail.com>
2020-10-22 15:30:41 -0500

    22 de Octubre

--- EdDyA II/Trees/BinarySearchTree.h
+++ EdDyA II/Trees/BinarySearchTree.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
 class Node{
   public: 
@@ -61,67 +62,26 @@ class BST{
       }
       return false;
     }
-    int remove(int data){
-      Node *a = root;
-      if( !a ) return -123;
-      while(a){
-	if(data == a -> data){
-	  Node *b = a;
-	  if(a -> left || a -> right){
-	    if(a -> left){
-	      a = a -> left;
-	      while(a -> right) a -> right;
-	    }
-	    else{
-	      a = a -> right;
-	      while(a -> left) a -> left;
-	    }
-
-	  }
-	  return a -> data;
-	}
-	else if(a -> left && data < a -> data) a = a -> left;
-	else if(a -> right && data > a -> data) a = a -> right;
-	else return false;
-      }
-      return false;
-    }
-
-    /*
-    friend ostream &operator << (ostream &output, const BST &n){
-      output << n.root -> data << endl;
-      if( n.root -> left ) output << n.root -> left;
-      if( n.root -> right ) output << n.root -> right;
-      return output;
-    }
-    */
 }; 
 void preOrden(Node *r){
-  cout << *r;
+  cout << r -> data << " ";
   if( r -> left ) preOrden(r -> left);
   if( r -> right ) preOrden(r -> right);
 }
 void enOrden(Node *r){
   if( r -> left ) enOrden(r -> left);
-  cout << *r;
+  cout << r -> data << " ";
   if( r -> right ) enOrden(r -> right);
 }
 void postOrden(Node *r){
   if( r -> left ) postOrden(r -> left);
   if( r -> right ) postOrden(r -> right);
-  cout << *r;
+  cout << r -> data << " ";
 }
-int main(){
-  BST b;
-  for(int i = -3; i < 12; i++)
-    b.insert(i);
-  preOrden(b.root);
-  cout << endl;
-  enOrden(b.root);
-  cout << endl;
-  postOrden(b.root);
-  cout << endl;
-  for(int i = -5; i < 15; i++)
-    cout << i << ":\t" << b.contains(i) << endl;
-  return 0;
+BST createRandom(int n = 15){
+  BST tree;
+  srand(time(NULL));
+  for(int i = 0; i < n; i++)
+    tree.insert(rand() % 100);
+  return tree;
 }
--- EdDyA II/Trees/Construir.cpp
+++ EdDyA II/Trees/Construir.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "BinarySearchTree.h"
 using namespace std;
 int main(){
+  //BST t = createRandom();
+  BST t;
+  t.insert(2);
+  t.insert(1);
+  preOrden(t.root);
+  cout << endl;
+  enOrden(t.root);
+  cout << endl;
+  postOrden(t.root);
+  cout << endl;
   return 0;
 }
