54b6d7ee341f7b9e4482b8e14b83a2e871e694db
LuisR-jpg <laloro08@hotmail.com>
2021-02-09 08:35:59 -0600

    09 de Febrero

--- EdDyAIII/UPdecrecienteImproved.cpp
+++ EdDyAIII/UPdecrecienteImproved.cpp
@@ -3,17 +3,25 @@ using namespace std;
 int main(){
   int n;
   cin >> n;
-  int arr[n+3];
-  vector<int> lis;
+  vector<int> v;
   for(int a, i = 0; i < n; i++){
     cin >> a;
-    if(lis.empty() || a < lis.back()) lis.push_back(a);
+    if(v.empty() || a < v.back()) v.push_back(a);
     else{
-      int l = 0, r = n.size() - 1, m;
-      do{
-	m = (l + r) / 2;
-	if(m && lis[m] >= 
+      int l = 0, r = v.size() - 1, m;
+      while(l != m || l != r || r != m){
+        m = (l + r)/2;
+        cout << m << " ";
+        if(v[m] <= a && (!m || v[m - 1] > a)){
+  	break;
+        }
+        else if(a > v[m]) r = m;
+        else l = m;
+      }
+  	v[m] = a;
+      cout << "hola" << endl;
     }
   }
+  cout << v.size() << endl;
   return 0;
 }
