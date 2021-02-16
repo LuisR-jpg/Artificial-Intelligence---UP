3ba4aee67f70882746726ec75a4831363d609e09
Lalito Robles <laloro08@hotmail.com>
2021-02-15 20:40:14 -0600

    15 de Febrero

--- EdDyAIII/UPCuantoDinero.cpp
+++ EdDyAIII/UPCuantoDinero.cpp
@@ -3,17 +3,13 @@ using namespace std;
 int main(){
   int n, m;
   cin >> n >> m;
-  int a[n+3], b[m+3] = {0}, c[m+3] = {1000};
+  int a[n+3], c[m+3];
   for(int i = 0; i <= m; i++) c[i] = 1000;
   for(int i = 0; i < n; i++) cin >> a[i];
-  b[0] = 1, c[0] = 0;
-  for(int i = 0; i < n; i++){
-    for(int j = 0; j + a[i] <= m; j++){
-      b[j+a[i]] += b[j];
+  c[0] = 0;
+  for(int i = 0; i < n; i++)
+    for(int j = 0; j + a[i] <= m; j++)
       c[j+a[i]] = min(c[j+a[i]], c[j] + 1);
-    }
-  }
-  //for(int i = 0; i <= m; i++) cout << c[i] << " ";
-  cout << c[m] << endl;
+  cout << c[m];
   return 0;
 }
