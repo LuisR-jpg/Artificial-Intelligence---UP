5f811420e267490289461d019cdadbdb463d6ce8
Lalito Robles <laloro08@hotmail.com>
2021-03-13 12:47:36 -0600

    13 de Marzo

--- EdDyAIII/Problemas/SumaleFenwick.cpp
+++ EdDyAIII/Problemas/SumaleFenwick.cpp
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+const int mxN = 1e5 + 10;
+ll t[mxN], a[mxN], n, q;
 void update(int i, int v){
   i++;
   while(i <= n){
@@ -7,8 +10,8 @@ void update(int i, int v){
     i += (i & -i);
   }
 }
-int query(int i){
-  int res = 0;
+ll query(int i){
+  ll res = 0;
   i++;
   while(i > 0){
     res += t[i];
@@ -18,13 +21,16 @@ int query(int i){
 }
 void build(){
   for(int i = 0; i < n; i++)
-    update(i, x[i]);
+    update(i, a[i]);
 }
 int main(){
-  int n;
-  cin >> n;
-  int a[n+3];
+  cin >> n >> q;
   for(int i = 0; i < n; cin >> a[i++]);
   build();
+  for(int x, y, p, i = 0; i < q; i++){
+    cin >> p >> x >> y;
+    if(p) cout << query(y) - (x? query(--x): 0) << endl;
+    else a[x] = y, update(x, 0);
+  }
   return 0;
 }
--- EdDyAIII/RMQ.cpp
+++ EdDyAIII/RMQ.cpp
@@ -2,23 +2,28 @@
 #define N 10001
 using namespace std;
 int x[N], m[N][25], q, n;
+int query(int a, int b){
+  int k = (int)log2(b - a + 1);
+  int vu = m[a][k], vd = m[b - (1 << k) + 1][k];
+  return (x[vu] < x[vd]? x[vu]: x[vd]);
+}
 int main(){
   cin >> n >> q;
-  memset(m, -1, sizeof(m));
   for(int i = 0; i < n; cin >> x[i++]);
   for(int i = 0; i < n; i++) m[i][0] = i;
-  for(int j = 1; (1 << j) < n; j++){
+  for(int j = 1; (1 << j) <= n; j++)
     for(int i = 0; i + (1 << j) - 1 < n; i++){
-      if(x[m[i][j-1]] < x[m[i + (1 << (j - 1) - 1)][j - 1]])
+      if(x[m[i][j-1]] < x[m[i + (1 << (j - 1))][j - 1]])
 	m[i][j] = m[i][j-1];
       else m[i][j] = m[i + (1 << (j - 1))][j - 1];
     }
-  }
   for(int i = 0; i < n; i++){
-    for(int j = 0; (1 << j) < n; j++){
+    for(int j = 0; (1 << j) <= n; j++){
       cout << m[i][j] << " ";
     }
     cout << endl;
   }
+  for(int i = 0, a, b; i < q; i++)
+    cin >> a >> b, cout << query(a, b) << endl;
   return 0;
 }
