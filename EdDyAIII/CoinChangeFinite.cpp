99b9cdf8070b958a7c87cd88d301096384d7c677
LuisR-jpg <laloro08@hotmail.com>
2021-03-25 21:03:21 -0600

    25 de Marzo

--- EdDyAIII/CoinChangeFinite.cpp
+++ EdDyAIII/CoinChangeFinite.cpp
@@ -1,1 +1,17 @@
-//-------------NO TERMINADO
+//Coin change. Finite coins
+//	Input: n and n coins.
+//	The i-th square contains the number of ways to sum i.
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  int n;
+  cin >> n;
+  int m = 1000; //m represents the maximum possible sum of the problem
+  int a[n+3] = {0}, b[m+3] = {0};
+  for(int i = 0; i < n; cin >> a[i++]);
+  b[0] = 1;
+  for(int i = 0; i < n; i++)
+    for(int j = m, c = a[i]; j >= c; j--)
+      b[j] += b[j-c];
+  return 0;  
+}
--- EdDyAIII/FenwickTree.cpp
+++ EdDyAIII/FenwickTree.cpp
@@ -1,4 +1,3 @@
-//------------NO TERMINADO
 //Fenwick or Binary Indexed Tree
 //Given information, it evaluates queries and updates values.
 //Ez to code
@@ -10,16 +9,19 @@
 //  Update O(logn)
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+const int MAX = 2e5 + 10;
+ll a[MAX], t[MAX], n, q;
 void update(int i, int v){
-  i++;
+  //i++;
   while(i <= n){
     t[i] += v;
     i += (i & -i);
   }
 }
-int query(int i){
-  int res = 0;
-  i++;
+ll query(ll i){
+  ll res = 0;
+  //i++;
   while(i > 0){
     res += t[i];
     i -= (i & -i);
@@ -28,13 +30,17 @@ int query(int i){
 }
 void build(){
   for(int i = 0; i < n; i++)
-    update(i, x[i]);
+    update(i + 1, a[i]);
 }
 int main(){
-  int n;
-  cin >> n;
-  int a[n+3];
+  cin >> n >> q;
   for(int i = 0; i < n; cin >> a[i++]);
   build();
+  for(int i = 0, f, g; i < q; i++){
+    cin >> f >> g;
+    f--;
+    if(g - 1 != f) cout << query(g) - query(f) << endl;
+    else cout << a[g-1] << endl;
+  }
   return 0;
 }
