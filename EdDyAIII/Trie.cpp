e576ec6ad283907a93604615e78b6f3ccc6e5162
LuisR-jpg <laloro08@hotmail.com>
2021-03-26 18:19:09 -0600

    26 de Marzo

--- EdDyAIII/Trie.cpp
+++ EdDyAIII/Trie.cpp
@@ -1,9 +1,8 @@
 //TRIE. Aka: Prefix Tree
+//	Data Structure used to find a word in a dictionary.
+//	Find word O(m) *let m be the length of the word you are looking for
 
-//Data Structure used to find a word in a dictionary.
-//Find word O(m) *let m be the length of the word you are looking for
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 class TrieNode{
   public:
@@ -15,36 +14,30 @@ class TrieNode{
     }
 };
 vector<TrieNode> Trie;
-void addWord(int nodeID, string word, int pos){
+void addWord(string word, int nodeID = 0, int pos = 0){
   if(pos == word.length()) return;
   int k = Trie[nodeID].ref[word[pos]-'a'];
   if(k == -1){
-    k = Trie.size();
-    Trie[nodeID].ref[word[pos]-'a'] = k;
+    Trie[nodeID].ref[word[pos]-'a'] = k = Trie.size();
     Trie.push_back(TrieNode());
   }
-  Trie[k].isWord = (pos == word.length()-1)? true: false;
-  addWord(k, word, pos + 1);
+  Trie[k].isWord = (pos == word.length() - 1 || Trie[k].isWord);
+  addWord(word, k, pos + 1);
 }
-bool search(int nodeID, string word, int pos){
-  if(Trie[nodeID].isWord && pos == word.length()) return 1;
+bool search(string word, int nodeID = 0, int pos = 0){
+  if(pos == word.length()) return Trie[nodeID].isWord;
   int k = Trie[nodeID].ref[word[pos]-'a'];
   if(k == -1) return 0;
-  return search(k, word, pos + 1);
+  return search(word, k, pos + 1);
 }
 int main(){
   Trie.push_back(TrieNode());
   int n, m;
-  cin>>n>>m;
-  string tosearch="";
-  for(int i=0; i<n; i++){
-  	cin>>tosearch;
-  	addWord(0,tosearch, 0);
-  }
-  for(int i=0; i<m; i++){
-  	cin>>tosearch;
-  	if(search(0, tosearch, 0) == 1) cout<<"YES"<<endl;
-  	else cout<<"NO"<<endl;
-  }
+  cin >> n >> m;
+  string a;
+  for(int i = 0; i < n; i++)
+    cin >> a, addWord(a);
+  for(int i=0; i<m; i++)
+    cin >> a, cout << (search(a)? "YES": "NO") << endl;
   return 0;
 }
