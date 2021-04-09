03a66ccfa0b065c8b08bc313866441a7fd556c58
LuisR-jpg <laloro08@hotmail.com>
2021-04-09 13:39:00 -0500

    9 de Abril

---
+++ EdDyAIII/Problemas/B.Dora.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m, da, db, y = 1;
+bool v[60][60];
+char s[60][60];
+int dfs(int i, int j){
+  if(i < 0 || i >= n || j < 0 || j >= m || v[i][j] || s[i][j] == '*') 
+    return 1000;
+  if(s[i][j] == 'E') return y = 0;
+  v[i][j] = 1;
+  return min(min(dfs(i + 1, j), dfs(i - 1, j)), min(dfs(i, j + 1), dfs(i, j - 1))) + 1;
+}
+int main(){
+  cin >> n >> m;
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < m; j++){
+      cin >> s[i][j];
+      if(s[i][j] == 'S') da = i, db = j;
+    }
+  }
+  memset(v, 0, sizeof(v));
+  int x = dfs(da, db);
+  cout << (y? -1: x);
+  return 0;
+}
