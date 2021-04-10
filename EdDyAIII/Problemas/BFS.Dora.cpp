756596b133fc8875f9adf33ac71672367a36ef07
LuisR-jpg <laloro08@hotmail.com>
2021-04-10 14:30:39 -0500

    10 de Abril

--- EdDyAIII/Problemas/BFS.Dora.cpp
+++ EdDyAIII/Problemas/BFS.Dora.cpp
@@ -8,7 +8,7 @@ int main(){
   char s[n+3][m+3];
   int v[n+3][m+3];
   memset(v, -1, sizeof(v));
-  for(int i = 0; i < n; i++)
+  for(int i = 0; i < n; i++){
     for(int j = 0; j < m; j++){
       cin >> s[i][j];
       if(s[i][j] == 'S')
@@ -16,30 +16,20 @@ int main(){
       if(s[i][j] == 'E')
 	s[i][j] = '.', x = i, y = j;
     }
-  queue<pair<int, pair<int, int>>> q;
-  q.push({0, {a, b}});
+  }
+  queue<pair<int, int>> q;
+  q.push({a, b}), v[a][b] = 0;
   while(q.size()){
-    //cout << "hola" << endl;
     auto p = q.front();
-    //cout << p.s.f << " " << p.s.s << endl;
     q.pop();
-    v[p.s.f][p.s.s] = p.f;
-    /*
-    for(int i = 0; i < n; i++){
-      for(int j = 0; j < m; j++){
-	cout << v[i][j] << " "; 
-      }
-      cout << endl;
-    }
-    */
-    if(p.s.f + 1 < n && s[p.s.f + 1][p.s.s] == '.' && v[p.s.f + 1][p.s.s] == -1)
-      q.push({p.f + 1, {p.s.f + 1, p.s.s}});
-    if(p.s.f - 1 >= 0 && s[p.s.f - 1][p.s.s] == '.' && v[p.s.f - 1][p.s.s] == -1)
-      q.push({p.f + 1, {p.s.f - 1, p.s.s}});
-    if(p.s.s + 1 < m && s[p.s.f][p.s.s + 1] == '.' && v[p.s.f][p.s.s + 1] == -1)
-      q.push({p.f + 1, {p.s.f, p.s.s + 1}});
-    if(p.s.s - 1 >= 0 && s[p.s.f][p.s.s - 1] == '.' && v[p.s.f][p.s.s - 1] == -1)
-      q.push({p.f + 1, {p.s.f, p.s.s - 1}});
+    if(p.f + 1 < n && s[p.f + 1][p.s] == '.' && !(v[p.f + 1][p.s] + 1))
+      q.push({p.f + 1, p.s}), v[p.f + 1][p.s] = v[p.f][p.s] + 1;
+    if(p.f > 0 && s[p.f - 1][p.s] == '.' && !(v[p.f - 1][p.s] + 1))
+      q.push({p.f - 1, p.s}), v[p.f - 1][p.s] = v[p.f][p.s] + 1;
+    if(p.s + 1 < m && s[p.f][p.s + 1] == '.' && !(v[p.f][p.s + 1] + 1))
+      q.push({p.f, p.s + 1}), v[p.f][p.s + 1] = v[p.f][p.s] + 1;
+    if(p.s > 0 && s[p.f][p.s - 1] == '.' && !(v[p.f][p.s - 1] + 1))
+      q.push({p.f, p.s - 1}), v[p.f][p.s - 1] = v[p.f][p.s] + 1;
   }
   cout << v[x][y];
   return 0;
