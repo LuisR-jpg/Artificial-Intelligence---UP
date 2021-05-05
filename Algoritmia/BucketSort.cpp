0c351e9b036ab8931f035e3271056935d2b91f43
LuisR-jpg <laloro08@hotmail.com>
2021-05-04 21:51:53 -0500

    04 de Mayo

--- Algoritmia/BucketSort.cpp
+++ Algoritmia/BucketSort.cpp
@@ -3,13 +3,13 @@ using namespace std;
 const int mN = 1e5;
 int main(){
   int n, a[mN] = {0}, mxN = INT_MIN;
+  cin >> n;
   for(int i = 0, x; i < n; i++){
     cin >> x;
     a[x]++, mxN = max(mxN, x);
   }
-  for(int i = 0; i < mN; i++) 
-    if(a[i])
-      for(int j = 0; j < mxN; j++)
+  for(int i = 0; i < 1 + mxN; i++) 
+      for(int j = 0; j < a[i]; j++)
 	cout << i << " ";
   return 0;
 }
