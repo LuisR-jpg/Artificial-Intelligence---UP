dd60e31d9729ab7b960bc77979413613919060d1
LuisR-jpg <laloro08@hotmail.com>
2021-09-27 19:50:49 -0500

    27 de Septiembre

--- Optimización y Metaheurísticas/NewtonRaphson.R
+++ Optimización y Metaheurísticas/NewtonRaphson.R
@@ -1,6 +1,6 @@
 library('Deriv')
 
-NewtonRaphson <- function(x, e){
+NewtonRaphson <- function(x, e, it = -1){
   xPrev = 0
   fDeriv = Deriv(f)
   sDeriv = Deriv(fDeriv)
@@ -12,6 +12,7 @@ NewtonRaphson <- function(x, e){
     xPlot <- c(xPrev, x) #Graphing
     
     lines(xPlot, f(xPlot), type = "b") #Graphing
+    if(!(it <- it - 1)) break
   }
   cat("x =", x, "\nf(x) =", f(x), "\nf'(x) =", fDeriv(x), "\nf''(x) =", sDeriv(x))
   return(matrix(v, ncol = 4, byrow = TRUE)) #CSV
@@ -22,9 +23,10 @@ grafica <- function(a, b){ #Graphing
   plot(x, f(x), type="l")
 }
 f <- function(x){
-  x^2 + 2*exp(-x)
+  x^2 + x^4
 }
-grafica(0, 2) 
-write.csv(NewtonRaphson(2, 0.001), file = "Git/School/Optimización y Metaheurísticas/NewtonRaphson.csv") #CSV
+#grafica(-5, 5) 
+plot(c(-5, 5, 0), c(20, 20, -2))
+write.csv(NewtonRaphson(-4, 1e-8, 10), file = "Git/School/Optimización y Metaheurísticas/NewtonRaphson.csv") #CSV
 
 
--- Optimización y Metaheurísticas/Secante.R
+++ Optimización y Metaheurísticas/Secante.R
@@ -1,32 +1,42 @@
 library('Deriv')
 
-Secante <- function(a, b, e){
+Secante <- function(a, b, e, it = -1){
   flag = 0
-  it = 1
   alpha = 0
   fDeriv = Deriv(f)
-  #h = c(it, alpha, f(alpha), fDeriv(alpha))
-  h = c()
+  h = xPlot = c() #CSV
   repeat{
     alpha = (a + b)/2
+    xPlot = c(xPlot, alpha)
     if(fDeriv(a)*fDeriv(alpha) < 0){
       b = alpha;
       break;
     }
     else a = alpha;
-    h = c(h, alpha, f(alpha), fDeriv(alpha))
+    h = c(h, alpha, f(alpha), fDeriv(alpha)) #CSV
+    if(!(it <- it - 1)) break
   }
   repeat{
     alpha = b - fDeriv(b)/(fDeriv(b) - fDeriv(a))/(b - a)
+    xPlot = c(xPlot, alpha)
     if(fDeriv(alpha) > 0) b = alpha
     else a = alpha
-    h = c(h, alpha, f(alpha), fDeriv(alpha))
+    h = c(h, alpha, f(alpha), fDeriv(alpha)) #CSV
     if(abs(fDeriv(alpha)) < e) break
+    if(!(it <- it - 1)) break
   }
-  return(matrix(h, ncol = 3, byrow = TRUE))
+  lines(xPlot, f(xPlot), type = "b")
+  return(matrix(h, ncol = 3, byrow = TRUE)) #CSV
+}
+grafica <- function(a, b){ #Graphing 
+  x = seq(a, b, 0.1)
+  plot(x, f(x), type="l")
 }
 f <- function(x){
-  x^2 + 1
+  x^2 + x^4
 }
 #Secante(-4, 3, 0.00000001)
-write.csv(Secante(-3, 4, 1e-8), file = "Git/School/Optimización y Metaheurísticas/Secante.csv") #CSV
+
+#grafica(-2, 2)
+plot(0, 0)
+write.csv(Secante(-4, 2, 1e-8, 10), file = "Git/School/Optimización y Metaheurísticas/Secante.csv") #CSV
