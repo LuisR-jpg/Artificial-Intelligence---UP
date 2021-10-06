be63a7c57f41838cf10d70a81c1d34bf3b76a05e
LuisR-jpg <laloro08@hotmail.com>
2021-10-06 09:53:57 -0500

    06 de Octubre

---
+++ Web/NodePrinciples/NodeandNPM/Mongoose/index.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+mongoose
+    .connect('mongodb://localhost:27017/daw')
+    .then(() => console.log('Probando Mongooo'))
+    .catch((err) => console.log("Error: ", err));
+
+const moiveSchema = new mongoose.Schema({
+    /*
+    title: "Toy Story", 
+    year: 1996, 
+    score: 100,
+    rating: 'A',    
+    */
+   title: String, 
+   year: Number,
+   score: Number, 
+   rating: String,
+});
+const Movie = mongoose.model("Movie", moiveSchema);
+const toystory = new Movie({title: "Toy Story", year: "1996", score: 100, rating: "A"});    
