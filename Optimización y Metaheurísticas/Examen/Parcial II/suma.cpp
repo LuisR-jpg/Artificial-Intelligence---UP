81570f9fe343e6494ffd40eab86b55e8498344d1
LuisR-jpg <laloro08@hotmail.com>
2021-10-22 17:30:59 -0500

    22 de Octubre

---
+++ Optimización y Metaheurísticas/Examen/Parcial II/suma.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  int n = 10, s = 0;
+  double a;
+  for(int i = 0; i < 10; i++){
+    cin >> a;
+    s += a;
+  }
+  cout << s; 
+  return 0;
+}
