68b2c8f17747d6fe3786054ba5b07e9d82012a3d
LuisR-jpg <laloro08@hotmail.com>
2021-11-01 21:22:20 -0600

    01 de Noviembre

---
+++ Probabilidad y Estadistica/Regresion/Ejercicios/Dos/Regresion_util.R
@@ -0,0 +1,74 @@
+# Universidad Panamericana
+# Campus Bonaterra
+# Departamento de Matemáticas
+# Probabilidad y Estadística para Ingeniería
+# 
+# Paul RC
+# 2021 11 01
+# =====
+# Archivo de utilerías para Regresión Lineal Simple
+# -----
+# Funciones
+# -----
+# Cálculo de los principales resultados de una Regresión Lineal Simple
+regresion_lineal_simple = function(datos, x_nueva=NULL) {
+  # Esta función asume que la primera columna contiene los valores para x y la segunda para y
+  x = datos[,1]
+  y = datos[,2]
+  n = dim(datos)[1]
+  # ---
+  # Calculamos beta_gorro0 y beta_gorro1
+  Sxx = sum(x^2) - 1/n*(sum(x))^2
+  Sxy = sum(x*y) - 1/n*(sum(x))*sum(y)
+  Syy = sum(y^2) - 1/n*(sum(y))^2
+  beta_gorro1 = Sxy/Sxx
+  beta_gorro0 = mean(y) - beta_gorro1*mean(x)
+  # Calculamos r y r^2
+  r = Sxy/sqrt(Sxx*Syy)
+  r2 = r^2
+  # Calculamos los residuos
+  y_gorro = beta_gorro0 + beta_gorro1*x
+  residuos = y - y_gorro
+  
+  # ---
+  # Gráfica de dispersión
+  plot(x, y, main="Gráfica de Dispersión",
+       xlab=names(datos)[1], ylab=names(datos)[2],
+       pch=16, col="blue")
+  # Agregamos la recta de regresión
+  abline(a=beta_gorro0, b=beta_gorro1, col="red")
+  
+  # ---
+  # Gráficas de residuos
+  par(mfrow=c(2,2))
+  # --
+  # ---
+  # Residuos estandarizados
+  residuosEstandarizados = (residuos - mean(residuos))/sd(residuos)
+  # ---
+  
+  # Dispersión de Residuos vs x
+  titulo = paste("Residuos vs", names(datos)[1])
+  plot(x, residuos, pch=16, col="green", main=titulo, xlab=names(datos)[1],
+       ylab="Residuos")
+  # Histograma de residuos
+  hist(residuos, main="Histograma de Residuos", xlab="Residuos", ylab="Frecuencia", col="green")
+  
+  
+  par(mfrow=c(1,1))
+  
+  # ---
+  # Pronóstico de y para x_nueva
+  if (! is.null(x_nueva)) {
+    y_nueva = beta_gorro0 + beta_gorro1*x_nueva
+  } else {
+    y_nueva = NULL
+  }
+  
+  # ---
+  # Devolvemos el resultado al programa principal
+  resultado = list(beta_gorro0=beta_gorro0, beta_gorro1=beta_gorro1,
+                   r=r, r2=r2, y_nueva=y_nueva)
+  
+  return(resultado)
+}
