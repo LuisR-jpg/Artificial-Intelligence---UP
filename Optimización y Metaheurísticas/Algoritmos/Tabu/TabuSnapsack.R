b076147f845de7407a36e18b2b3a5ebd8e677281
LuisR-jpg <laloro08@hotmail.com>
2021-11-08 19:18:29 -0600

    08 de Noviembre

--- Optimización y Metaheurísticas/Algoritmos/Tabu/TabuSnapsack.R
+++ Optimización y Metaheurísticas/Algoritmos/Tabu/TabuSnapsack.R
@@ -2,7 +2,10 @@
 tabu <- function(it = 15, tabuS = 2, pesos, beneficios, sol = c(), weightL, printIt = FALSE){
   h = c()
   bProfit = 0
+  bSol = c()
   tabuT = seq(0, 0, length.out = tabuS)
+  if(length(sol) == 0)
+    sol = seq(0, 0, length.out = length(pesos))
   for(i in 1: it){
     newProfit = -1
     newIndex = -1
@@ -28,10 +31,20 @@ tabu <- function(it = 15, tabuS = 2, pesos, beneficios, sol = c(), weightL, prin
   if(!printIt) print(h)
   return(bProfit)
 }
-tabu(
-  tabuS =      3,
-  pesos =      c(7, 12, 8, 9, 8, 6, 11, 5),
-  beneficios = c(10, 14, 9, 8, 7, 5, 9, 3),
-  sol =        c(1, 0, 0, 1, 1, 1, 0, 1),
-  weightL =    38
+# tabu(
+#   it =         30, 
+#   tabuS =      4,
+#   pesos =      c(7, 12, 8, 9, 8, 6, 11, 5),
+#   beneficios = c(10, 14, 9, 8, 7, 5, 9, 3),
+#   sol =        c(1, 0, 0, 1, 1, 1, 0, 1),
+#   weightL =    38,
+#   printIt =    FALSE
+# )
+tabu( 
+  it =    15,
+  pesos = c(4990, 1142, 7390, 10372, 3114, 2744, 3102, 7280, 2624, 3020, 2310, 2078, 3926, 9656, 32708, 4830, 2034, 4766, 40006),
+  beneficios = c(1945, 321, 2945, 4136, 1107, 1022, 1101, 2890, 962, 1060, 805, 689, 1513, 3878, 13504, 1865, 667, 1833, 16553),
+  tabuS = 4, 
+  weightL = 31181, 
+  printIt = FALSE
 )
