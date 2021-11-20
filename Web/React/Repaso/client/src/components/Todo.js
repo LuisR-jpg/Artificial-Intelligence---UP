de5ce143799dbe1042aee3833ada6df3e639dc1f
LuisR-jpg <laloro08@hotmail.com>
2021-11-20 00:28:26 -0600

    20 de Noviembre

--- Web/React/Repaso/client/src/components/Todo.js
+++ Web/React/Repaso/client/src/components/Todo.js
@@ -1,5 +1,5 @@
-import {useEffect} from "react";
-import { getTodos } from "../functions/index.js";
+import React, {useEffect, useState} from "react";
+import {getTodos} from "../functions/index.js";
 function Todo(){
     useEffect(() => {
         console.log("Todos");
@@ -9,6 +9,18 @@ function Todo(){
         };
         fetchData();
     }, []);
+    const [titleValue, setTitle] = useState();
+    const [descValue, setDesc] = useState();
+    function titleChanged(e){
+        const {value} = e.target;
+        setTitle(value);
+        console.log(titleValue);
+    }
+    function descChanged(e){
+        const {value} = e.target;
+        setDesc(value);
+        console.log(descValue);
+    }
     return(
         <div>
             <div className="row">
@@ -16,12 +28,12 @@ function Todo(){
                     <div className="row">
                         <div className="input-field col s6">
                             <i className="material-icons prefix">account_circle</i>
-                            <input id="icon_prefix" type="text" className="validate" />
+                            <input id="icon_prefix" type="text" className="validate" onChange = {titleChanged}/>
                             <label htmlFor="icon_prefix">Titulo</label>
                         </div>
                         <div className="input-field col s6">
                             <i className="material-icons prefix">Description</i>
-                            <input id="icon_telephone" type="tel" className="validate" />
+                            <input id="icon_telephone" type="tel" className="validate" onChange = {descChanged}/>
                             <label htmlFor="icon_telephone">Description</label>
                         </div>
                     </div>
