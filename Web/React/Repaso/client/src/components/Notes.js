9b25229490f6edb1767f765beeb16dc24fac7503
LuisR-jpg <laloro08@hotmail.com>
2021-11-22 10:35:40 -0600

    22 de Noviembre

--- Web/React/Repaso/client/src/components/Notes.js
+++ Web/React/Repaso/client/src/components/Notes.js
@@ -1,19 +1,20 @@
 import {getTodos} from "../functions/index.js";
-import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, {useEffect, useState} from "react";
 function Notes(){
     var rows = [];
+    const [notas, setNotas] = useState();
 
     useEffect(() => {
         console.log("Todos");
         const fetchData = async() => {
             const result = await getTodos();
             console.log('Fetched data', result);
-
+            setNotas(result);
         };
         fetchData();
     }, []);
@@ -22,29 +23,18 @@ function Notes(){
     rows.push(<Col>{i} of 5</Col>);
     return(
         <>  
-            <Container style = {{backgroundColor: "wheat"}}>
-
-            <Row>
-                {rows}
-                {rows}
-                {rows}
-                {rows}
-                {rows}
-                {rows}
-                {rows}
-                {rows}
-                {rows}
-                {rows}
-                {rows}
-                {rows}
-                {rows}
-                {rows}
-                {rows}
-                <Col>1 of 3</Col>
-                <Col>2 of 3</Col>
-                <Col>2 of 2</Col>
-                <Col>3 of 3</Col>
-            </Row>
+            <Container>
+                    {notas && notas.map((nota) => {
+                        if(nota.title)
+                            return(
+                                <Card>
+                                    <Card.Header>{nota.title}</Card.Header>
+                                    <Card.Body>
+                                        <Card.Text>{nota.content}</Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            )
+                    })}
             </Container>
         </>
     );
