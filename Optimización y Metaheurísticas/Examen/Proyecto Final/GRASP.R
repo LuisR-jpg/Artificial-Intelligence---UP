2dfbdaa596f6381a77c25919c45c8e15c3182a90
LuisR-jpg <laloro08@hotmail.com>
2021-12-01 20:30:47 -0600

    01 de Diciembre

--- Optimización y Metaheurísticas/Examen/Proyecto Final/GRASP.R
+++ Optimización y Metaheurísticas/Examen/Proyecto Final/GRASP.R
@@ -1,12 +1,10 @@
+#Lectura
 myFile = read.delim(file.choose(), header = FALSE, sep = " ")
-#myFile = read.delim("C:\\Users\\lalor\\Documents\\Git\\School\\Optimización y Metaheurísticas\\Examen\\Proyecto Final\\Instancias_sjupm\\Mine\\Instancia1.sjupm", header = FALSE, sep = " ")
 nOperaciones = myFile[1, 1]
 nMaquinas = myFile[1, 2]
 rows = 1 + nMaquinas
-#unlist(myFile[2: 4, 1: 10])
 tOperaciones = matrix(unlist(myFile[2: rows, 1: nOperaciones]), byrow = FALSE, nrow = nMaquinas)
 tAjuste = list()
-
 for(i in 1: nMaquinas){
   nRow = 2 + nMaquinas + nOperaciones*(i - 1)
   nRowE = nRow + nOperaciones - 1
@@ -14,28 +12,26 @@ for(i in 1: nMaquinas){
   tAjuste[[i]] = m
 }
 
-#print(nOperaciones)
-#print(nMaquinas)
-#print(tOperaciones)
-#cat("tiempo de ajuste\n")
-#print(length(tAjuste))
+#Fase #1. Greedy Randomized Adaptive
 Greedy <- function(){
-  top = 3
-  matAvg = Reduce('+', tAjuste)/nMaquinas
-  vecAvg = colSums(tOperaciones)/nMaquinas
-  matAvg = matAvg + vecAvg[col(matAvg)]
+  #alpha
+  top = 3 
+  #matriz de promedios
+  matAvg = Reduce('+', tAjuste)/nMaquinas  
+  vecAvg = colSums(tOperaciones)/nMaquinas 
+  matAvg = matAvg + vecAvg[col(matAvg)] 
   diag(matAvg) = Inf
+  #Guarda el indice de la operación más rápida en la maquina mas rapida
   minOp = arrayInd(which.min(tOperaciones), dim(tOperaciones))
-  assignedOp = c()
+  assignedOp = c() 
   sol = vector(mode = "list", length = nMaquinas)
   times = rep(0, nMaquinas)
   currOp = minOp[2]
-  #print(matAvg)
   repeat {
+    #Operación elegida 
     assignedOp = c(assignedOp, currOp)
-    #cat("NUEVA ITERACION", "\n")
-    #print(currOp)
     vecValidation = c()
+    #Se guarda la operación en cada maquina
     for(i in 1: nMaquinas){
       suma = times[i] + tOperaciones[1, currOp]
       nOM = length(sol[[i]])
@@ -43,16 +39,14 @@ Greedy <- function(){
       vecValidation = c(vecValidation, suma)
     }
     assignedM = which.min(vecValidation)
-    #print("Sol")
-    #print(times + tOperaciones[, currOp])
-    #print(which.min(times + tOperaciones[, currOp]))
     sol[[assignedM]] = c(sol[[assignedM]], currOp)
     times[assignedM] = times[assignedM] + tOperaciones[assignedM, currOp]
-    nOM = length(sol[[assignedM]]) #Número de operaciones en la máquina asignada
+    nOM = length(sol[[assignedM]]) #Numero de operaciones en la maquina asignada
     if(nOM > 1)
       times[assignedM] = times[assignedM] + tAjuste[[assignedM]][sol[[assignedM]][nOM - 1], sol[[assignedM]][nOM]]
     if(length(assignedOp) == nOperaciones) break
     
+    #Crea lista de vecinos
     vecFila = matAvg[currOp, ]
     neighborhood = c()
     for(i in 1: nOperaciones){
@@ -70,18 +64,14 @@ Greedy <- function(){
   }
   return(sol)
 }
-#sol           #Solucion Inicial
 
-
+#Tiempo que toma cada maquina 
 timeList <- function(solution, prosecutionTime, adjustmentTime){
   times = c()
-  #print(length(solution))
   for(i in 1:length(solution)){
-    #cat("i",i)
     time = 0
     for(j in 1:length(solution[[i]]))
     {
-      #cat("j",j)
       time = time + prosecutionTime[i,solution[[i]][j]]
       if(j >= 2)
       {
@@ -93,11 +83,12 @@ timeList <- function(solution, prosecutionTime, adjustmentTime){
   return(times)
 }
 
+#Tiempo que toma una maquina en específico
 evaluar <- function(vec, indice, prosecutionTime, maquinas){
   tiempo = 0
   for(i in 1: length(vec)){
     tiempo = tiempo + prosecutionTime[indice, vec[i]]
-    if(i!=length(vec)){                                     #No agrego el tiempo de ajuste de la última operacion
+    if(i!=length(vec)){                                     #No agrego el tiempo de ajuste de la ultima operacion
       tiempo = tiempo + maquinas[[indice]][vec[i],vec[i+1]]
     }
   }
@@ -105,12 +96,12 @@ evaluar <- function(vec, indice, prosecutionTime, maquinas){
 }
 
 
-
+# Fase 2. Busqueda local con Tabu
 local_search_tabu <- function(iter, initialSol, tenor){
   lista_tabu = c()
   tenor_tabu = tenor
   
-  best_of_the_best = initialSol
+  best_of_the_best = initialSol #la solucion inicial es la mejor solucion hasta ahora
   
   for(i in 1:iter){
     index_peor = which.max(timeList(initialSol,tOperaciones,tAjuste))
@@ -126,13 +117,12 @@ local_search_tabu <- function(iter, initialSol, tenor){
         
         if(!(initialSol[[index_peor]][i] %in% lista_tabu)){
           vec_aux = initialSol[[index_mejor]]  
-          vec_aux = append(vec_aux, initialSol[[index_peor]][i] ,j)      #Agregando operacion a una posicion de la maquina menor
-          #cat("nueva solucion -> ",vec_aux,"\n")
+          vec_aux = append(vec_aux, initialSol[[index_peor]][i] ,j)      #Agrega operacion a una posicion de la maquina menor
           
           aux = evaluar(vec_aux, index_mejor, tOperaciones, tAjuste)
-          #cat("solucion evaluada",aux,"\n")
           
-          if(aux < best){
+          #si es mejor que el mejor anterior, lo tomamos y actualizamos 
+          if(aux < best){ 
             quitar = i
             operacion = initialSol[[index_peor]][i]
             best = aux
@@ -142,14 +132,11 @@ local_search_tabu <- function(iter, initialSol, tenor){
       }
     }
     
-    #cat("mejor solucion",sol," valor:", best, " y se agrega en el tabu como: Maquina y posicion que hay que bloquear", operacion)
-    
     mejor_solucion = initialSol
     index = c(quitar)
-    mejor_solucion[[index_peor]]<-mejor_solucion[[index_peor]][-index]    #quitarle a la peor maquina la operacion
+    mejor_solucion[[index_peor]] <- mejor_solucion[[index_peor]][-index]    #Le quita a la peor maquina la operacion
     mejor_solucion[[index_mejor]] = sol
     initialSol = mejor_solucion
-    #print(initialSol)   
     
     #Comparacion
     maximo1 = max(timeList(initialSol,tOperaciones,tAjuste))
@@ -157,6 +144,7 @@ local_search_tabu <- function(iter, initialSol, tenor){
     if(maximo1<maximo2)best_of_the_best = initialSol
     
     
+    #Añade la operacion a la lista tabu para evitar repetir
     lista_tabu <- append(lista_tabu, operacion)
     if(length(lista_tabu) > tenor_tabu){
       indice <- c(1)
@@ -165,15 +153,15 @@ local_search_tabu <- function(iter, initialSol, tenor){
     
   }
   
-  return (best_of_the_best)
+  return (best_of_the_best) #regresa la mejor solucion
 }
 
 soluciones = c()
 bSolution = c()
-for(i in 1: 50){
+#Se repite el proceso 50 veces
+for(i in 1: 50){ 
   mejor = local_search_tabu(150, Greedy(), 2)
   tiempo = max(timeList(mejor,tOperaciones,tAjuste))
-  #print(mejor)
   soluciones = c(soluciones, tiempo)
   if(tiempo == min(soluciones)) 
     bSolution = mejor
@@ -181,11 +169,12 @@ for(i in 1: 50){
 soluciones
 
 
+#Graficas de Gantt
+
+#Instalar las siguientes dos líneas:
 # install.packages('devtools')
 # devtools::install_github('Kitware/candela', subdir='R/candela', dependencies = TRUE)
-
 library(candela)
-
 buildDiagrms <- function(solution, prosecutionTime, adjustmentTime)
 {
   data = list()
@@ -198,12 +187,10 @@ buildDiagrms <- function(solution, prosecutionTime, adjustmentTime)
     else different = 2
     for(m in 1:length(solution[[n]]))
     {
-      #print(data)
       if(m >= 2)
       {
         data[[i]] = list(name=gsub(" ", "", paste("A",solution[[n]][m-1])), level=different, start=time, end=time+adjustmentTime[[n]][solution[[n]][m-1],solution[[n]][m]])
         time = time + adjustmentTime[[n]][solution[[n]][m-1],solution[[n]][m]]
-        #data[[i]] = list(name=gsub(" ", "", paste("A",solution[[n]][m-1])), level=different, start=time-adjustmentTime[[n]][solution[[n]][m-1],solution[[n]][m]], end=time)
         i = i + 1
       }
       data[[i]] = list(name=gsub(" ", "", paste("O",solution[[n]][m])), level=different, start=time, end=time+prosecutionTime[n,solution[[n]][m]])
@@ -220,4 +207,3 @@ candela('GanttChart',
         data=data, label='name',
         start='start', end='end', level='level')
 cat(mean(soluciones), sd(soluciones), min(soluciones), max(soluciones))
-
