c8c48bc003e7976909eed97c9be6d70f5308fb5c
LuisR-jpg <laloro08@hotmail.com>
2021-12-01 18:14:57 -0600

    01 de Diciembre

--- Optimización y Metaheurísticas/Examen/Proyecto Final/GRASP.R
+++ Optimización y Metaheurísticas/Examen/Proyecto Final/GRASP.R
@@ -175,8 +175,49 @@ for(i in 1: 50){
   tiempo = max(timeList(mejor,tOperaciones,tAjuste))
   #print(mejor)
   soluciones = c(soluciones, tiempo)
-  if(tiempo == max(soluciones)) 
+  if(tiempo == min(soluciones)) 
     bSolution = mejor
 }
 soluciones
+
+
+# install.packages('devtools')
+# devtools::install_github('Kitware/candela', subdir='R/candela', dependencies = TRUE)
+
+library(candela)
+
+buildDiagrms <- function(solution, prosecutionTime, adjustmentTime)
+{
+  data = list()
+  i = 1
+  different = 0
+  for(n in 1:length(solution))
+  {
+    time = 0
+    if(n %% 2 == 1) different = 1
+    else different = 2
+    for(m in 1:length(solution[[n]]))
+    {
+      #print(data)
+      if(m >= 2)
+      {
+        data[[i]] = list(name=gsub(" ", "", paste("A",solution[[n]][m-1])), level=different, start=time, end=time+adjustmentTime[[n]][solution[[n]][m-1],solution[[n]][m]])
+        time = time + adjustmentTime[[n]][solution[[n]][m-1],solution[[n]][m]]
+        #data[[i]] = list(name=gsub(" ", "", paste("A",solution[[n]][m-1])), level=different, start=time-adjustmentTime[[n]][solution[[n]][m-1],solution[[n]][m]], end=time)
+        i = i + 1
+      }
+      data[[i]] = list(name=gsub(" ", "", paste("O",solution[[n]][m])), level=different, start=time, end=time+prosecutionTime[n,solution[[n]][m]])
+      time = time + prosecutionTime[n,solution[[n]][m]]
+      i = i + 1
+    }
+  }
+  return(data)
+}
+
+data = buildDiagrms(bSolution,tOperaciones,tAjuste)
+
+candela('GanttChart',
+        data=data, label='name',
+        start='start', end='end', level='level')
 cat(mean(soluciones), sd(soluciones), min(soluciones), max(soluciones))
+
