88edea26b61f838efafb33eb7649f36fabc0019f
LalitoR-jpg <laloro08@hotmail.com>
2022-01-31 15:58:34 -0600

    31 de Enero

---
+++ Programacion de Agentes y Entornos Virtuales/FirstProject/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX, movementY;
+    public float speed = 0;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void OnMove(InputValue movementValue){
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+    void FixedUpdate(){
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement*speed);
+    }
+}
