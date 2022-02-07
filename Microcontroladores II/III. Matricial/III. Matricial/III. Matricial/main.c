63f91938518079863014120d0bb4d06b7fb7e40a
LalitoR-jpg <laloro08@hotmail.com>
2022-02-07 12:14:39 -0600

    07 de Febrero

--- Microcontroladores II/III. Matricial/III. Matricial/III. Matricial/main.c
+++ Microcontroladores II/III. Matricial/III. Matricial/III. Matricial/main.c
@@ -6,7 +6,6 @@
  */ 
 
 #define F_CPU 1000000
-#define print(n) ((n % 10) | ((n / 10) << 4))
 #define isClear(r, i) (!(r & (1 << i)))
 #define isSet(r, i) (r & (1 << i))
 #define PINX PINA
@@ -16,24 +15,38 @@
 #include <stdint.h>
 #include <util/delay.h>
 
-uint8_t keyboard[4][4] = 
+//uint8_t keyboard[4][4] = 
+//{
+	//{0x7, 0x8, 0x9, 0xA},
+	//{0x4, 0x5, 0x6, 0xB},
+	//{0x1, 0x2, 0x3, 0xC},
+	//{0xE, 0x0, 0xF, 0xD}
+//};
+uint8_t keyboard[4][4] =
 {
-	{0x7, 0x8, 0x9, 0xA},
+	{0x1, 0x2, 0x3, 0xA},
 	{0x4, 0x5, 0x6, 0xB},
-	{0x1, 0x2, 0x3, 0xC},
+	{0x7, 0x8, 0x9, 0xC},
 	{0xE, 0x0, 0xF, 0xD}
 };
+uint8_t tecla, c = 0, d = 0, n[] = {0, 0, 0};
 
+void print(){
+	for(uint8_t i = 0; i < 3; i++){
+		PORTB = (n[i] | (1 << (5 + i)));
+	}
+}
 uint8_t hastaTecla(){
-	for(uint8_t i = 0;; i++){
-		if(i == 4) i = 0;
+	for(uint8_t i = 0;; i++, i %= 4){
+		print();
 		PORTX = ~(1 << i);
 		asm("nop");
 		asm("nop");
 		for(uint8_t j = 4; j < 8; j++){
 			if(isClear(PINX, j)){
 				_delay_ms(50);
-				while(isClear(PINX, j));
+				while(isClear(PINX, j))
+					print();
 				_delay_ms(50);
 				return keyboard[7 - j][3 - i];
 			}
@@ -41,16 +54,25 @@ uint8_t hastaTecla(){
 	}
 }
 
-int main(){
-    DDRX = 0x0F;
-    PORTX = 0xFF;
+void start(){
+	tecla = c = d = n[0] = n[1] = n[2] = 0;
+	DDRX = 0x0F;
+	PORTX = 0xFF;
 	DDRC = 0xFF;
 	PORTC = 0x00;
 	DDRD = 0xFF;
 	PORTD = 0x00;
-    uint8_t tecla, c = 0, d = 0;
+	DDRB = 0xFF;
+}
+int main(){
+	start();
     for(;;){
 	    tecla = hastaTecla();
+		if(tecla == 0xF){
+			start();
+			continue;
+		}
+		n[2] = n[1], n[1] = n[0], n[0] = tecla;
 		d = c >> 4;
 		c <<= 4;
 		c |= tecla;
