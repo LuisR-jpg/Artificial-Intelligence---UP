2d4cc83fc512333df33d6ddc0e14eadad8df019f
LalitoR-jpg <laloro08@hotmail.com>
2022-02-23 18:46:17 -0600

    23 de Febrero
    Sí?
    
    23 de Febrero

---
+++ Programacion de Agentes y Entornos Virtuales/2DGame/Proyecto1/Assets/Scripts/Magic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magic : MonoBehaviour
+{
+    public GameObject cosa;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        cosa.SetActive(false); 
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        cosa.SetActive(true);
+    }
+}
---
+++ Programacion de Agentes y Entornos Virtuales/2DGame/Proyecto1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem; 
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    private float movX, movY;
+    public float speed = 1; 
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>(); 
+    }
+
+    private void OnMove(InputValue movValue)
+    {
+        Vector2 movVect = movValue.Get<Vector2>();
+        movX = movVect.x;
+        movY = movVect.y;
+        bool mov = true;
+
+        if (movX > 0) anim.SetInteger("direction", 1);
+        if (movX < 0) anim.SetInteger("direction", 3);
+        if (movY > 0) anim.SetInteger("direction", 2);
+        if (movY < 0) anim.SetInteger("direction", 0);
+
+        if (!Keyboard.current.wKey.isPressed &&
+            !Keyboard.current.aKey.isPressed &&
+            !Keyboard.current.sKey.isPressed &&
+            !Keyboard.current.dKey.isPressed) mov = false;
+
+        anim.SetBool("walking", mov);
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 mov = new Vector2(movX, movY);
+        rb.velocity = mov * speed;
+    }
+}
