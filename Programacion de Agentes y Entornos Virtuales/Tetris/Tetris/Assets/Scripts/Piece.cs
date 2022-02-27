11ac269afad9a60945b34982b2ea0f577f7ef0be
LalitoR-jpg <laloro08@hotmail.com>
2022-02-27 00:51:58 -0600

    27 de Febrero

--- Programacion de Agentes y Entornos Virtuales/Tetris/Tetris/Assets/Scripts/Piece.cs
+++ Programacion de Agentes y Entornos Virtuales/Tetris/Tetris/Assets/Scripts/Piece.cs
@@ -8,11 +8,19 @@ public class Piece : MonoBehaviour {
     public Vector3Int position {get; private set;}
     public int rotationIndex {get; private set;}
 
+    public float stepDelay = 1f;
+    public float lockDelay = 0.5f;
+
+    private float stepTime;
+    private float lockTime;
+
     public void Initialize(Board board, Vector3Int position, TetrominoData data) {
         this.board = board;
         this.position = position;
         this.data = data;
         this.rotationIndex = 0;
+        this.stepTime = Time.time + this.stepDelay;
+        this.lockTime = 0f;
         if(this.cells == null){
             this.cells = new Vector3Int[data.cells.Length];
         }
@@ -22,6 +30,9 @@ public class Piece : MonoBehaviour {
     }
     private void Update(){
         this.board.Clear(this);
+
+        this.lockTime += Time.deltaTime;
+
         if(Input.GetKeyDown(KeyCode.Q)){
             Rotate(-1);
         }
@@ -40,10 +51,28 @@ public class Piece : MonoBehaviour {
         if(Input.GetKeyDown(KeyCode.Space)){
             HardDrop();
         }
+
+        if(Time.time >= this.stepTime){
+            Step();
+            Move(Vector2Int.down);
+            if(this.lockTime >= this.lockDelay){
+                Lock();
+            }
+        }
+
         this.board.Set(this);
+    }
+    private void Step(){
+        this.stepTime = Time.time + this.stepDelay;
+    }
+    private void Lock(){
+        this.board.Set(this);
+        this.board.ClearLines();
+        this.board.SpawnPiece();
     }
     private void HardDrop(){
         while(Move(Vector2Int.down));
+        Lock();
     }
     private bool Move(Vector2Int translation){
         Vector3Int newPosition = this.position;
@@ -52,17 +81,59 @@ public class Piece : MonoBehaviour {
         bool valid = this.board.IsValidPosition(this, newPosition);
         if(valid){
             this.position = newPosition;
+            this.lockTime = 0f;
         }
         return valid;
     }
     public void Rotate(int direction){
+        int originalRotation = this.rotationIndex;
         this.rotationIndex = Wrap(this.rotationIndex + direction, 0, 4);
-        for(int i = 0; i < this.cells.Lenght; i++){
+        ApplyRotationMatrix(direction);
+        if(!TestWallkicks(this.rotationIndex, direction)){
+            this.rotationIndex = originalRotation;
+            ApplyRotationMatrix(-direction);
+        } 
+    }
+
+    private void ApplyRotationMatrix(int direction){
+        for(int i = 0; i < this.cells.Length; i++){
             Vector3 cell = this.cells[i];
             int x, y;
-            
+            switch(this.data.tetromino){
+                case Tetromino.I:
+                case Tetromino.O:
+                    cell.x -= 0.5f;
+                    cell.y -= 0.5f;
+                    x = Mathf.CeilToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.CeilToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    break;
+                default:
+                    x = Mathf.RoundToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.RoundToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    break;
+            }
+            this.cells[i] = new Vector3Int(x, y, 0);
         }
     }
+
+    private bool TestWallkicks(int rotationIndex, int rotationDirection){
+        int wallKickIndex = GetWallKickIndex(rotationIndex, rotationDirection);
+        for(int i = 0; i < this.data.WallKicks.GetLength(1); i++){
+            Vector2Int translation = this.data.WallKicks[wallKickIndex, i];
+            if(Move(translation))
+                return true;
+        }
+        return false;
+    }
+
+    private int GetWallKickIndex(int rotationIndex, int rotationDirection){
+        int wallKickIndex = rotationIndex * 2;
+        if(rotationDirection < 0){
+            wallKickIndex--;
+        }
+        return Wrap(wallKickIndex, 0, this.data.WallKicks.GetLength(0));
+    }
+
     private int Wrap(int input, int min, int max){
         if(input < min) return max - (min - input) % (max - min);
         else return min + (input - min) % (max - min);
--- Programacion de Agentes y Entornos Virtuales/Tetris/Tetris/Assets/Scripts/Tetromino.cs
+++ Programacion de Agentes y Entornos Virtuales/Tetris/Tetris/Assets/Scripts/Tetromino.cs
@@ -1,13 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.Tilemaps;
 public enum Tetromino{
-    I, 
-    O, 
-    T,
-    J,
-    L,
-    S,
-    Z
+    I, O, T, J, L, S, Z
 }
 
 [System.Serializable]
@@ -15,7 +9,10 @@ public struct TetrominoData{
     public Tetromino tetromino;
     public Tile tile;
     public Vector2Int[] cells {get; private set;}
+    public Vector2Int[,] WallKicks {get; private set;}
+
     public void Initialize() {
         this.cells = Data.Cells[this.tetromino];
+        this.WallKicks = Data.WallKicks[this.tetromino];
     }
 }
