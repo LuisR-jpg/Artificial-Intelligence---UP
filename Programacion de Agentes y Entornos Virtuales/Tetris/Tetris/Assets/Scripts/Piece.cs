6f2a48b5b68049e8ea25dcbd08a6f3f9ba05b61f
LalitoR-jpg <laloro08@hotmail.com>
2022-02-28 08:32:11 -0600

    Input system improved

--- Programacion de Agentes y Entornos Virtuales/Tetris/Tetris/Assets/Scripts/Piece.cs
+++ Programacion de Agentes y Entornos Virtuales/Tetris/Tetris/Assets/Scripts/Piece.cs
@@ -10,9 +10,11 @@ public class Piece : MonoBehaviour {
 
     public float stepDelay = 1f;
     public float lockDelay = 0.5f;
+    public float inputDelay = 0.2f;
 
     private float stepTime;
     private float lockTime;
+    private float inputTime;
 
     public void Initialize(Board board, Vector3Int position, TetrominoData data) {
         this.board = board;
@@ -32,25 +34,31 @@ public class Piece : MonoBehaviour {
         this.board.Clear(this);
 
         this.lockTime += Time.deltaTime;
+        
+        if(Time.time >= this.inputTime){
+            Sensibility();
 
-        if(Input.GetKeyDown(KeyCode.Q)){
-            Rotate(-1);
-        }
-        else if(Input.GetKeyDown(KeyCode.E)){
-            Rotate(1);
-        }
-        if(Input.GetKeyDown(KeyCode.A)){
-            Move(Vector2Int.left);
-        }
-        else if(Input.GetKeyDown(KeyCode.D)){
-            Move(Vector2Int.right);
-        }
-        if(Input.GetKeyDown(KeyCode.S)){
-            Move(Vector2Int.down);
+            if(Input.GetKey(KeyCode.Q))
+                Rotate(-1);
+            else if(Input.GetKey(KeyCode.E))
+                Rotate(1);
+            if(Input.GetKey(KeyCode.A))
+                Move(Vector2Int.left);
+            else if(Input.GetKey(KeyCode.D))
+                Move(Vector2Int.right);
+            if(Input.GetKey(KeyCode.S))
+                Move(Vector2Int.down);
         }
         if(Input.GetKeyDown(KeyCode.Space)){
             HardDrop();
         }
+        if(!(
+            Input.GetKey(KeyCode.Q) ||
+            Input.GetKey(KeyCode.E) ||
+            Input.GetKey(KeyCode.A) ||
+            Input.GetKey(KeyCode.D) ||
+            Input.GetKey(KeyCode.S)
+        )) this.inputTime = Time.time;
 
         if(Time.time >= this.stepTime){
             Step();
@@ -64,6 +72,9 @@ public class Piece : MonoBehaviour {
     }
     private void Step(){
         this.stepTime = Time.time + this.stepDelay;
+    }
+    private void Sensibility(){
+        this.inputTime = Time.time + this.inputDelay;
     }
     private void Lock(){
         this.board.Set(this);
