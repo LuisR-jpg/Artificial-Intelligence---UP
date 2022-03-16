b49049a51fe50373a5a163f4fd255ec36eab720b
LalitoR-jpg <laloro08@hotmail.com>
2022-03-15 18:37:54 -0600

    15 de Marzo

---
+++ Programacion de Agentes y Entornos Virtuales/3D/Assets/FPSController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+    Action control;
+    Action.FpsMapActions actions;
+    private Vector2 mouseVector;
+
+    private void Awake(){
+        control = new Action();
+        actions = control.fpsMap;
+        actions.MoveCamera.performed += ctx => mouseVector = ctx.ReadValue<Vector2>();
+    }
+
+    private void OnEnable(){
+        control.Enable();
+    }
+
+    private void OnDestroy(){ 
+        control.Disable();
+    }
+
+    void Update()
+    {
+        print(mouseVector);
+    }
+}
---
+++ Programacion de Agentes y Entornos Virtuales/3D/Assets/OldController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OldController : MonoBehaviour
+{
+    public float a = 50f;
+    public float force = 10f;
+    private Transform camT;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        camT = transform.GetChild(0); //Tomamos el primer hijo
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float vRot = Input.GetAxis("Mouse Y") * a;
+        float hRot = Input.GetAxis("Mouse X") * a;
+        print(vRot + ", " + hRot);
+        float vax = Input.GetAxis("Vertical");
+        float hax = Input.GetAxis("Horizontal");
+        transform.Rotate(0f, hRot * Time.deltaTime, 0f);
+        camT.Rotate(vRot * Time.deltaTime, 0f, 0f);
+        rb.AddForce(((transform.forward * vax) + (transform.right * hax)) * force);
+    }
+}
