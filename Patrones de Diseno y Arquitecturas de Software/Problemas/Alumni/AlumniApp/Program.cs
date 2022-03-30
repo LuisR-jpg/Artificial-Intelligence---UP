766a458c6b2fd8fbd8435f059feb1e5bf2c46314
LalitoR-jpg <laloro08@hotmail.com>
2022-03-29 23:26:20 -0600

    30 de Marzo

--- Patrones de Diseno y Arquitecturas de Software/Problemas/Alumni/AlumniApp/Program.cs
+++ Patrones de Diseno y Arquitecturas de Software/Problemas/Alumni/AlumniApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,6 +15,7 @@ namespace AlumniApp
         [STAThread]
         static void Main()
         {
+            Console.WriteLine(ConfigurationManager.AppSettings["exportTo"]);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Welcome());
