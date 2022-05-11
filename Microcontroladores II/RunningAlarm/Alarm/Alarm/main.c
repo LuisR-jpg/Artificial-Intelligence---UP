ebc094c9a15329a1fab2d1fb3cd5a6d3d1bb41b3
LalitoR-jpg <laloro08@hotmail.com>
2022-05-11 09:24:46 -0500

    11 de Mayo

---
+++ Microcontroladores II/RunningAlarm/Alarm/Alarm/main.c
@@ -0,0 +1,18 @@
+/*
+ * Alarm.c
+ *
+ * Created: 8/5/2022 15:50:39
+ * Author : lalor
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
