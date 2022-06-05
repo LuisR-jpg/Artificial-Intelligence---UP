7e0e3bb6e9e99a5f2485b7cafa05b216a8fc8348
LalitoR-jpg <laloro08@hotmail.com>
2022-06-05 13:03:31 -0700

    06 de Junio

---
+++ Programacion de Agentes y Entornos Virtuales/3D/FPS Chess/ChessTest/Assets/Scripts/Board.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+
+    public GameObject whiteCell;
+    public GameObject blackCell;
+    void Start()
+    {
+        //Instantiate(whiteCell, new Vector3(0, 0, 0), Quaternion.identity);
+
+        // GameObject a = Instantiate(blackCell);
+        // Vector3 newRotation = new Vector3(-90, 0, 0);
+        // a.transform.position = new Vector3(0.5f, 0, 0.5f);
+        // a.transform.eulerAngles = newRotation;
+        GameObject cell;
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                cell = Instantiate(((i + j) % 2 == 1)? blackCell: whiteCell);
+                cell.name = (char)(i + 'A') + j.ToString();
+                cell.transform.position = new Vector3(i - 3.5f, 0, j - 3.5f);
+                cell.transform.eulerAngles = new Vector3(-90, 0, 0);
+            }
+        }
+    }
+
+}
