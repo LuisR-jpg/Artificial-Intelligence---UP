c5db680c4e7585007c454103d1e4b33f7fae262c
LuisR-jpg <laloro08@hotmail.com>
2022-08-27 00:43:46 -0500

    27 de Agosto

--- Desarrollo de Aplicaciones Moviles/Xamarin/Calculator/Calculator/MainPage.xaml.cs
+++ Desarrollo de Aplicaciones Moviles/Xamarin/Calculator/Calculator/MainPage.xaml.cs
@@ -14,5 +14,37 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string operation = Convert.ToString(Operation.SelectedItem);
+            if (!string.IsNullOrEmpty(One.Text) && !string.IsNullOrEmpty(Two.Text) && !string.IsNullOrEmpty(operation))
+            {
+                double a = Convert.ToDouble(One.Text);
+                double b = Convert.ToDouble(Two.Text);
+                double result = 0;
+                switch (operation)
+                {
+                    case "+":
+                        result = a + b;
+                        break;
+                    case "-":
+                        result = a - b;
+                        break;
+                    case "//":
+                        result = a / b;
+                        break;
+                    case "*":
+                        result = a * b;
+                        break;
+                }
+                DisplayAlert("Calculator", One.Text + " " + operation + " " + Two.Text + " = " + Convert.ToString(result), "Ok");
+            }
+            else
+            {
+                DisplayAlert("Wrong data",
+                    "Please fill all the info", "Ok");
+            }
+        }
     }
 }
