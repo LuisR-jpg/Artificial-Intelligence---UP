5e1e9ae41af28f18f73c6f89af6a8c50928b1c91
LuisR-jpg <laloro08@hotmail.com>
2022-08-26 23:31:23 -0500

    26 de Agosto

---
+++ Desarrollo de Aplicaciones Moviles/IMCApp/IMCApp/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IMCApp
+{
+    public partial class App : Application
+    {
+        public App ()
+        {
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart ()
+        {
+        }
+
+        protected override void OnSleep ()
+        {
+        }
+
+        protected override void OnResume ()
+        {
+        }
+    }
+}
---
+++ Desarrollo de Aplicaciones Moviles/IMCApp/IMCApp/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace IMCApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Height.Text) &&
+                !string.IsNullOrEmpty(Weight.Text))
+            {
+                var altura = double.Parse(Height.Text);
+                var peso = double.Parse(Weight.Text);
+                var imc = peso / (altura * altura);
+                IMC.Text = imc.ToString();
+            }
+            else
+            {
+                DisplayAlert("Wrong data",
+                    "Please fill all the info", "Ok");
+            }
+        }
+    }
+}
