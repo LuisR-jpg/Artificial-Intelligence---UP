84539fad4cf95388eb10bb6da9b25e6caed4e809
LuisR-jpg <laloro08@hotmail.com>
2023-02-14 16:11:58 -0600

    14 de Febrero

---
+++ Compiladores/Compilador/AnalizadorLexico/src/codigo/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package codigo;
+
+import java.io.File;
+
+/**
+ *
+ * @author lalor
+ */
+public class Principal {
+    public static void main(String[] args) {
+        String ruta = "C:\\Users\\lalor\\Documents\\Git\\School\\Compiladores\\Compilador\\AnalizadorLexico\\src\\codigo\\Lexer.flex";
+        generarLexer(ruta);
+    }
+    public static void generarLexer(String ruta){
+        File archivo = new File(ruta);
+        JFlex.Main.generate(archivo);
+    }
+}
---
+++ Compiladores/Compilador/AnalizadorLexico/src/codigo/Tokens.java
@@ -0,0 +1,24 @@
+package codigo;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+
+/**
+ *
+ * @author lalor
+ */
+
+
+public enum Tokens {
+    Reservadas,
+    Igual,
+    Suma,
+    Resta,
+    Multiplicacion,
+    Division,
+    Identificador,
+    Numero,
+    ERROR
+}
