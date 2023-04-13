c107fc619cd3ab127ad1a8af0d929ff7e907ee2b
LuisR-jpg <laloro08@hotmail.com>
2023-04-13 15:39:04 -0500

    13 de Abril

--- Compiladores/230. Prefix Posfix/Prefix_Posfix_Stack.java
+++ Compiladores/230. Prefix Posfix/Prefix_Posfix_Stack.java
@@ -3,86 +3,94 @@ import java.io.*;
 
 public class Prefix_Posfix_Stack {
 
-    private static Stack tokenize(String expression) {
+    private static Stack<String> tokenize(String expression) {
         Stack s = new Stack();
-
+        for (int i = expression.length() - 1; i >= 0; i--) 
+            s.push("" + expression.charAt(i));
         return s;
     }
 
     public static void main(String[] args) {
-        Stack s = new Stack();
-        Stack<Character> operando = new Stack<>();
-        Stack<Character> operadores = new Stack<>();
+        // Get expression
         Scanner input = new Scanner(System.in);
-        String expresion;
-        int res=0;
+        System.out.println("Ingrese una expresión:");
+        String expresion = input.nextLine();
+        
+        Stack<String> formula = tokenize(expresion);
+        Stack<Double> operando = new Stack<>();
+        Stack<Character> operadores = new Stack<>();
+        int res = 0;
         Map<Character, Integer> priorities = Map.of(
-                ')', 1,
-                '^', 2,
-                '*', 3,
-                '/', 3,
-                '+', 4,
-                '-', 4
+                ')', 40,
+                '^', 30,
+                '*', 20,
+                '/', 20,
+                '+', 10,
+                '-', 10
         );
 
-        // Get expression
-        System.out.print("Ingrese una expresión:\n");
-        expresion = input.nextLine();
 
-        // Store expression
-        for (int i = expresion.length() - 1; i >= 0; i--) {
-            s.push(expresion.charAt(i));
-        }
+        while (!formula.empty()) {
+            String x = formula.pop();
+            
+            char operator;
 
-        while (!s.empty()) {
-            String x = s.peek();
-            if (Character.isDigit(x)) {                           //operando
-                operando.push(x);
-            } else {                                                  //operador
-                // Caso 1 y 2: vacío || "("
-                if (operadores.empty() || x == '(') {
-                    operadores.push(x);
-                } else {
-                    char operador_top = operadores.peek();
-                    if (priorities.get(x) > priorities.get(operador_top)) {
-                        operadores.push(x);
-                    }else{
-                        int a = Character.getNumericValue(operando.pop());
-                        int b = Character.getNumericValue(operando.pop());
-                        int op = (char)operadores.pop();
-                        System.out.print(a+" "+b+" "+op);
-                        switch(op){
-                            case '^':
-                                operando.push((char)(a^b));
-                                break;
-                            case '*':
-                                operando.push((char)(a*b));
-                                break;
-                            case '/':
-                                operando.push((char)(a/b));
-                                break;
-                            case '+':
+            System.out.println(x);
+
+            // It's digit
+            try {
+                operando.push(Double.parseDouble(x));
+                System.out.println("Digit");
+                continue;
+            } catch(NumberFormatException e) {
+                assert x.length() == 1: String.format("(%s) no es reconocido como operador.", x);
+                operator = x.charAt(0);
+            }
+            
+            // Caso 1 y 2: vacío || "("
+            if (operadores.empty() || operator == '(') 
+                operadores.push(operator);
+            else if (!operadores.empty()) {
+                char operador_top = operadores.peek();
+                if (priorities.get(operator) > priorities.get(operador_top)) {
+                    operadores.push(operator);
+                    continue;
+                }
+                else{
+                    Double b = operando.pop();
+                    Double a = operando.pop();
+                    char op = operadores.pop();
+                    System.out.print(a+" "+b+" "+op);
+                    switch(op){
+                        case '^':
+                            operando.push(Math.pow(a, b));
+                            break;
+                        case '*':
+                            operando.push(a*b);
+                            break;
+                        case '/':
+                            operando.push(a/b);
+                            break;
+                        case '+':
+                            break;
 //                                int resultado = a + b;
 //                                char resultadoChar = Character.forDigit(resultado, 10);
 //                                operando.push(resultadoChar);
 //                                operadores.pop();
 //                                System.out.print("\n"+resultadoChar);
-                                break;
-                            case '-':
-                                operando.push((char)(a-b));
-                                break;
-                            default:
-                                System.out.print("\nNo conozco ese operador. Sorry.");
-                                break;
-                        }
-                        
-                        
+                        case '-':
+                            operando.push(a-b);
+                            break;
+                        default:
+                            System.out.print("\nNo conozco ese operador. Sorry.");
+                            break;
                     }
                 }
-
             }
-            s.pop();
-            
+            else if (operator == ')'){
+                continue;
+            }
         }
+        if(!operando.empty()) System.out.println(operando.pop());
     }
 }
